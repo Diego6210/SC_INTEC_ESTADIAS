cf4edc26f54cd14ec0f1a4207850d9f70867137e
DIEGO <diegoalberto621@outlook.com>
2021-01-04 17:33:46 -0600

    Prueba de PDF

--- application/libraries/M_pdf.php
+++ application/libraries/M_pdf.php
@@ -1,10 +1,9 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 // libreria actualizada con un error en el servidor
-// require_once __DIR__ . '/vendor/autoload.php';
- require_once __DIR__ . '/vendor-mpdf-v3/autoload.php';
+// require_once __DIR__ . '/vendor-mpdf-v3/autoload.php';
 
-// require_once __DIR__ . '/vendor-mpdf/autoload.php';
+require_once __DIR__ . '/vendor-mpdf/autoload.php';
 
 class M_pdf {
 
